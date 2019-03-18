9ab0b8abd099eb663d7eb90609907679db2ec0b3
sherwin wyco <sherwin.wyco20@gmail.com>
2019-03-18 16:53:31 -0400

    added start menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
+import javax.swing.JFrame;
+
+import panel.GamePanel;
+
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JFrame window = new JFrame("Dungeon Ninja");
+		window.setContentPane(new GamePanel());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);
 	}
 
 }
